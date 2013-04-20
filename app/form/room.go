cefb36086ecee8244cd2a79eb9d397a1451a5e3b
lidashuang <ldshuang@gmail.com>
2013-04-21 05:45:40 +0800

    add room form

--- app/form/room.go
+++ app/form/room.go
@@ -1,4 +1,15 @@
 package form
+
 import (
     "github.com/robfig/revel"
 )
+
+type roomForm struct {
+    Name string
+    Desc string
+}
+
+func (rf *roomForm) Validation(v *revel.Validation) {
+    v.Required(rf.Name)
+    v.Required(rf.Desc)
+}
